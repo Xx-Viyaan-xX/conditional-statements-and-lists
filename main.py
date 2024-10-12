4aef7005af6da9435242a41d9811f8d2133d49ba
Xx_Viyaan_xX <viyaantheking@gmail.com>
2024-10-12 15:23:44 +0530

    if else conditsioms

---
+++ main.py
@@ -0,0 +1,13 @@
+number=int(input("enter a number to check if it is positive negitive or neutral"))
+print("check the number",number)
+
+if number>0: 
+    print("it is a positive number")
+
+elif number<0: 
+    print("it is a negitive number")
+
+else:
+    print("it is a neutral number")
+
+
